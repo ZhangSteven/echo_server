36a51e52881a272bed3f36518b6b5523e4dff559
Clamc User <cuser@ubuntu-test01.clamc.com.hk>
2019-03-03 09:50:46 +0800

    URL /echo/<xxx> is ready

--- app.py
+++ app.py
@@ -2,6 +2,13 @@
 # 
 # The main Flask app
 # 
+# The "/" and "/datetime" is for testing purpose, make sure the server is
+# reachable.
+# 
+# To measure network round trip time, use the "/echo/<requestNum>" URL, such as
+# 
+# http://<server_name>/echo/1
+# 
 from flask import Flask
 from datetime import datetime
 app = Flask(__name__)
@@ -20,6 +27,12 @@ def getDatetime():
 
 
 
+@app.route('/echo/<requestNum>')
+def echo(requestNum):
+	return 'response {0}'.format(requestNum)
+
+
 
 if __name__ == '__main__':
+	# accept incoming requests from anywhere
 	app.run(host='0.0.0.0', port=80, debug=True)
